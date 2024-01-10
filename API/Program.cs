de4560969e9d6572655952d874325d2069b06e3b
GETRONICS\cmachin <cecilia.machin@getronics.com>
2024-01-10 14:42:56 +0100

    End of section 4

--- API/Program.cs
+++ API/Program.cs
@@ -1,16 +1,18 @@
+using System.Text;
 using API.Data;
+using API.Extensions;
+using API.Interfaces;
+using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(opt =>
-{
-   opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-}
-);
-builder.Services.AddCors();
+builder.Services.AddApplicationService(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -37,6 +39,8 @@ app.MapGet("/weatherforecast", () =>
 .WithName("GetWeatherForecast");
 .WithOpenApi();*/
 app.UseCors(builder=>builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+app.UseAuthentication();
+app.UseAuthorization();
 app.MapControllers();
 app.Run();
 
