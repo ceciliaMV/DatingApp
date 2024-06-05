ae0cbfea338ba323db2265d64a6800cc1b67d34e
GETRONICS\cmachin <cecilia.machin@getronics.com>
2024-06-05 11:10:19 +0200

    End of section 7

--- API/Program.cs
+++ API/Program.cs
@@ -1,11 +1,8 @@
 using System.Text;
 using API.Data;
 using API.Extensions;
-using API.Interfaces;
-using API.Services;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
+using API.Middleware;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,7 +12,11 @@ builder.Services.AddApplicationService(builder.Configuration);
 builder.Services.AddIdentityServices(builder.Configuration);
 var app = builder.Build();
 
+
 // Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
+app.UseCors(builder=>builder.AllowAnyHeader().AllowAnyMethod()
+.WithOrigins("https://localhost:4200"));
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
