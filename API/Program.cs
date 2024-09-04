9557802b67f37c43b3458d92d59a79f82c61e9b4
GETRONICS\cmachin <cecilia.machin@getronics.com>
2024-09-04 11:26:59 +0200

    End of section 8

--- API/Program.cs
+++ API/Program.cs
@@ -2,6 +2,7 @@ using System.Text;
 using API.Data;
 using API.Extensions;
 using API.Middleware;
+using Microsoft.EntityFrameworkCore;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -43,6 +44,16 @@ app.UseCors(builder=>builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try{
+   var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedUsers(context);
+}catch(Exception ex){
+    var logger =services.GetService<ILogger<Program>>();
+    logger.LogError(ex,"An error occurred during migration");
+}
 app.Run();
 
 /*record WeatherForecast(DateOnly Date, int TemperatureC, string Summary)
